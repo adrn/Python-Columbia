8642778d8cd67664d6abc327a3f5e9d2e0805e3e
adrn <adrian.prw@gmail.com>
2012-04-27 13:18:56 -0400

    redesign structure for /examples/ subdir of website

---
+++ NamespaceExample1.py
@@ -0,0 +1,11 @@
+import os
+
+def print_list(the_list):
+    """ Given a list of values, print each value on a new line """
+    for item in the_list:
+        print(item)
+
+a = 15
+b = 25
+print_list([a,b])
+    
